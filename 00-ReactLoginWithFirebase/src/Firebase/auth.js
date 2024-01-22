1db4bbb760e4367400fa659cda5954db26935e74
Benjamin Nicolas Montero Torres <benjamin.montero@globant.com>
2024-01-22 01:43:41 -0300

    Create new project 0-ReactLoginWithFirebase - Installation of Vite and cleanup to set up the new project

---
+++ 00-ReactLoginWithFirebase/src/Firebase/auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvALOPVI6ae1aWiSJiTSbkld7eL4cJfl4",
+  authDomain: "react-login-96031.firebaseapp.com",
+  projectId: "react-login-96031",
+  storageBucket: "react-login-96031.appspot.com",
+  messagingSenderId: "325910210102",
+  appId: "1:325910210102:web:ea4d26ace1bf8ee60d9a97"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
